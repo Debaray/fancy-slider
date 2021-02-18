fea7e7c72d153f13cee44f137b70e3a6dc5737bf
Debashis Ray <debaray14@gmail.com>
2021-02-18 21:21:16 +0600

    done

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const selectedImageTag = document.getElementById('selected-image');
 const selectedImageCount = document.getElementById('selected-image-count');
 const noDataDiv = document.getElementById('no-data-found');
 const createSlideDuration = document.getElementById('create-slide-duration');
+const containerMain = document.getElementById('container-main');
 
 // selected image 
 let sliders = [];
-selectedImageTag.style.display ='none';
+selectedImageTag.style.display = 'none';
 
 // If this key doesn't work
 // Find the name in the url and go to their website
@@ -21,47 +22,46 @@ selectedImageTag.style.display ='none';
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 //Enter Press
-searchBox.addEventListener('keypress',function(event){
-  if(event.key =='Enter')
-  {
+searchBox.addEventListener('keypress', function (event) {
+  if (event.key == 'Enter') {
     searchBtn.click();
   }
-}) 
+})
 
-sliderDurationBox.addEventListener('keypress',function(event){
-  if(event.key =='Enter')
-  {
+sliderDurationBox.addEventListener('keypress', function (event) {
+  if (event.key == 'Enter') {
     sliderBtn.click();
   }
-}) 
+})
 
-const displayControl =(element, display) =>{
- element.style.display =`${display}`;
+const displayControl = (element, display) => {
+  element.style.display = `${display}`;
 }
 
 // show images 
 const showImages = (images) => {
-  if(images.length === 0)
-  {
+  if (images.length === 0) {
     isDisplayMealEmpty();
     return;
   }
-  displayControl(imagesArea,'block');
-  displayControl(noDataDiv,'none');
-  displayControl(createSlideDuration,'flex');
-  displayControl(gallery,'flex');
   
+
   gallery.innerHTML = '';
   // show gallery title
-  displayControl(galleryHeader,'flex');
+  displayControl(galleryHeader, 'flex');
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
-    displayControl(selectedImageTag,'block');
-    
-    selectedImageCount.innerText =sliders.length; 
+    displayControl(selectedImageTag, 'block');
+
+    selectedImageCount.innerText = sliders.length;
+    displayControl(imagesArea, 'block');
+  displayControl(noDataDiv, 'none');
+  displayControl(createSlideDuration, 'flex');
+  displayControl(gallery, 'flex');
+  displayControl(containerMain,'none');
   })
 
 }
@@ -70,7 +70,7 @@ const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
-    .catch(err => console.log(err))
+    .catch(err => isDisplayMealEmpty())
 }
 
 let slideIndex = 0;
@@ -78,15 +78,15 @@ const selectItem = (event, img) => {
 
   let element = event.target;
   element.classList.add('added');
- 
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
-    selectedImageCount.innerText =sliders.length; 
+    selectedImageCount.innerText = sliders.length;
   } else {
     sliders.splice(item, 1);
     element.classList.remove('added');
-    selectedImageCount.innerText =sliders.length; 
+    selectedImageCount.innerText = sliders.length;
   }
 }
 var timer
@@ -98,10 +98,7 @@ const createSlider = () => {
   }
   const duration = document.getElementById('duration').value || 1000;
   const isDurationInteger = parseFloat(duration);
-  console.log(isDurationInteger,Number.isInteger(isDurationInteger));
-
-  if(!Number.isInteger(isDurationInteger) == true || isDurationInteger <= 0) 
-  {
+  if (!Number.isInteger(isDurationInteger) == true || isDurationInteger <= 0) {
     alert('Duration should be positive integer number!!!!');
     return;
   }
@@ -115,13 +112,14 @@ const createSlider = () => {
   `;
 
   sliderContainer.appendChild(prevNext)
-  document.querySelector('.main').style.display = 'block';
-  // hide image aria
-  displayControl(imagesArea,'none');
-  displayControl(selectedImageTag,'none');
+  displayControl(containerMain,'block');
+  // hide image area
+  displayControl(imagesArea, 'none');
+  displayControl(selectedImageTag, 'none');
+  displayControl(noDataDiv, 'none');
 
-  selectedImageCount.innerText ="0"; 
-  
+  selectedImageCount.innerText = "0";
+
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
@@ -164,24 +162,25 @@ const changeSlide = (index) => {
 }
 
 const isDisplayMealEmpty = () => {
- 
-  displayControl(noDataDiv,'block');
-  displayControl(gallery,'none');
-  displayControl(createSlideDuration,'none');
-  displayControl(selectedImageTag,'none');
-      selectedImageCount.innerText ="0"; 
-      const createNoDataDiv = document.createElement('div');
-      noDataDiv.innerHTML = "";
-      const noDataInfo = `
+
+  
+  const createNoDataDiv = document.createElement('div');
+  noDataDiv.innerHTML = "";
+  const noDataInfo = `
       <div class="mt-5 mb-5 d-flex justify-content-center">
       <img src="images/noDataFound.webp">
       </div>
      `;
-      createNoDataDiv.innerHTML = noDataInfo;
-      noDataDiv.appendChild(createNoDataDiv);
+  createNoDataDiv.innerHTML = noDataInfo;
+  noDataDiv.appendChild(createNoDataDiv);
+  displayControl(noDataDiv, 'block');
+  displayControl(gallery, 'none');
+  displayControl(createSlideDuration, 'none');
+  displayControl(selectedImageTag, 'none');
+  selectedImageCount.innerText = "0";
 }
 searchBtn.addEventListener('click', function () {
-  document.querySelector('.main').style.display = 'none';
+  displayControl(containerMain,'none');
   clearInterval(timer);
   const search = document.getElementById('search');
   getImages(search.value)
